4900c739257bf621298ce4ca41d20bd6e8824f77
Naushad Ali <na864724@gmail.com>
2024-10-03 02:22:12 +0530

    first commit

---
+++ add.java
@@ -0,0 +1,9 @@
+public class add
+{
+    public static void main(String[] args) {
+        int a=20;
+        int b=30;
+        int c=a+b;
+        System.out.println("the addition is " +c);
+     }
+}
